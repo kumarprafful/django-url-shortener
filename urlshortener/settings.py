81792c0ba6f79d83df582a357f104b686f697c8e
Prafful Kumar <kumarprafful.54@gmail.com>
2020-01-20 17:25:03 +0530

    static files anaged

--- urlshortener/settings.py
+++ urlshortener/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [STATIC_DIR,]
+# STATICFILES_DIRS = [STATIC_DIR,]
 STATIC_ROOT = 'static/'
 
 SITE_URL = 'http://fidly.in/'
