b7e45e3bbb2275db592c22a76acaced11fab98b7
Prafful Kumar <kumarprafful.54@gmail.com>
2020-01-20 16:29:28 +0530

    to deploy

--- urlshortener/settings.py
+++ urlshortener/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'tadgr)*$$srlw0@kexyof-o^lextsy^ymg$)&3ov65+pna68^l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
