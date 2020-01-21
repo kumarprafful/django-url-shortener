f0d637b7c7497a12a2dfdaa9111f73e4656dc2f7
Prafful Kumar <kumarprafful.54@gmail.com>
2020-01-21 13:59:23 +0530

    active/inactive, edge case

--- shortener/views.py
+++ shortener/views.py
@@ -1,3 +1,4 @@
+import validators
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
@@ -13,7 +14,7 @@ def shorten_url(request):
     if request.method == 'POST':
         try:
             url = request.data['url']
-            if not url == '':
+            if not url == '' and validators.url(url):
                 short_id = get_short_id()
                 url_obj, _ = URLs.objects.get_or_create(long_url= url,
                                                             defaults={
@@ -23,11 +24,13 @@ def shorten_url(request):
                                                                 })
                 serializer = URLSerializer(url_obj)
                 return Response(serializer.data, status=200)
+            else:
+                return Response({"error": "URL not valid"}, status=400)
         except:
             return Response({"error": "bad request"} ,status=400)
 
 def redirect_to_original_url(request, short_id):
-    long_url_obj = get_object_or_404(URLs, pk=short_id)
+    long_url_obj = get_object_or_404(URLs, pk=short_id, active=True)
     long_url_obj.count += 1
     long_url_obj.save()
     return HttpResponseRedirect(long_url_obj.long_url)
