809fa73bb279b0cd1b266cfef97d1fda7ffe74a0
Prafful Kumar <kumarprafful.54@gmail.com>
2020-01-22 17:37:32 +0530

    edge cases

--- shortener/shorten.py
+++ shortener/shorten.py
@@ -6,8 +6,7 @@ def get_short_id():
     char = string.ascii_uppercase + string.digits + string.ascii_lowercase
     i = 0
     while i <= 1000:
-        # short_id = ''.join(random.choice(char) for x in range(length))
-        short_id = 'C54Wc8'
+        short_id = ''.join(random.choice(char) for x in range(length))
         try:
             temp = URLs.objects.get(pk=short_id)
             i += 1
